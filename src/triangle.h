73299da474a29c76da898e9cd989f3084111cd45
Alan <108598059@ntut.org.tw>
2020-07-25 16:56:53 +0800

    implement triangle ut

--- src/triangle.h
+++ src/triangle.h
@@ -3,13 +3,47 @@
 
 #include "shape.h"
 #include "two_dimensional_vector.h"
+#include <math.h>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-class Triangle : Shape{
+class Triangle : public Shape{
     public: 
-		Triangle(vector<TwoDimensionalVector> vectors);
+		Triangle(vector<TwoDimensionalVector*> vectors):_vectors(vectors){
+      if(vectors.size() != 3) {
+        throw string("this is not a triangle!");
+      }
+    }
+
+    // Triangle(vector<int> t){}
+    
+    
+    double area() const {
+      return fabs(
+              _vectors[0]->getX() * _vectors[1]->getY()
+               + _vectors[1]->getX() * _vectors[2]->getY() 
+               + _vectors[2]->getX() * _vectors[0]->getY() 
+               - _vectors[0]->getX() * _vectors[2]->getY() 
+               - _vectors[1]->getX() * _vectors[0]->getY() 
+               - _vectors[2]->getX() * _vectors[1]->getY()) / 2;
+    }
+
+    double perimeter() const {
+      return sqrt(pow(_vectors[0]->getX() - _vectors[1]->getX(), 2) + pow(_vectors[0]->getY() - _vectors[1]->getY(), 2))
+           + sqrt(pow(_vectors[0]->getX() - _vectors[2]->getX(), 2) + pow(_vectors[0]->getY() - _vectors[2]->getY(), 2)) 
+           + sqrt(pow(_vectors[2]->getX() - _vectors[1]->getX(), 2) + pow(_vectors[2]->getY() - _vectors[1]->getY(), 2));
+    }
+    
+    string type() const {
+      return "triangle";
+    }
+
+
+
+    private:
+    vector<TwoDimensionalVector*> _vectors;
 };
 
 #endif
--- test/ut_triangle.h
+++ test/ut_triangle.h
@@ -1,5 +1,59 @@
 #include "../src/triangle.h"
 
-TEST(Triangle, First){
-    ASSERT_TRUE(true);
+
+
+class TriangleTest: public testing::Test {
+    protected:
+    void SetUp() {
+        triangleVector.push_back(new TwoDimensionalVector(0, 0));
+        triangleVector.push_back(new TwoDimensionalVector(3, 0));
+        triangleVector.push_back(new TwoDimensionalVector(0, 4));
+    }
+    vector<TwoDimensionalVector*> triangleVector;
+};
+
+
+TEST_F(TriangleTest, ConstructorNoException){
+    ASSERT_NO_THROW(new Triangle(triangleVector));
+}
+
+TEST(Triangle, ExceptionForVectorLessThanThree){
+    vector<TwoDimensionalVector*> triangleExceptionVector;
+    triangleExceptionVector.push_back(new TwoDimensionalVector(3, 0));
+    triangleExceptionVector.push_back(new TwoDimensionalVector(0, 4));
+    try {
+        new Triangle(triangleExceptionVector);
+        FAIL();
+    }catch(string e) {
+        ASSERT_EQ("this is not a triangle!", e);
+    }
+}
+
+TEST(Triangle, ExceptionForVectorMoreThanThree){
+    vector<TwoDimensionalVector*> triangleExceptionVector;
+    triangleExceptionVector.push_back(new TwoDimensionalVector(3, 0));
+    triangleExceptionVector.push_back(new TwoDimensionalVector(0, 4));
+    triangleExceptionVector.push_back(new TwoDimensionalVector(-3, 0));
+    triangleExceptionVector.push_back(new TwoDimensionalVector(0, -4));
+    try {
+        new Triangle(triangleExceptionVector);
+        FAIL();
+    }catch(string e) {
+        ASSERT_EQ("this is not a triangle!", e);
+    }
+}
+
+TEST_F(TriangleTest, Area){
+    Shape* triangle = new Triangle(triangleVector);
+    ASSERT_EQ(6, triangle->area());
+}
+
+TEST_F(TriangleTest, Perimeter){
+    Shape* triangle = new Triangle(triangleVector);
+    ASSERT_EQ(12, triangle->perimeter());
+}
+
+TEST_F(TriangleTest, Type){
+    Shape* triangle = new Triangle(triangleVector);
+    ASSERT_EQ("triangle", triangle->type());
 }
