7d25b536db02a6a5882105dbb90b66915961f681
Alan <108598059>
2020-09-17 21:01:31 +0800

    fix ellipse test case assert near

--- test/ut_ellipse.h
+++ test/ut_ellipse.h
@@ -42,27 +42,27 @@ TEST(Ellipse, ExceptionForSemiMinorAxesLessThanZero){
 }
 
 TEST(Ellipse, AreaInt){
-    double abs = 0.0001;
+    double abs = 0.001;
     Shape* ellipse = new Ellipse(3, 4);
-    ASSERT_NEAR(37.6991, ellipse->area(), abs);
+    ASSERT_NEAR(37.699, ellipse->area(), abs);
 }
 
 TEST(Ellipse, AreaDouble){
-    double abs = 0.0001;
+    double abs = 0.001;
     Shape* ellipse = new Ellipse(3.7, 4.2);
-    ASSERT_NEAR(48.8203, ellipse->area(), abs);
+    ASSERT_NEAR(48.820, ellipse->area(), abs);
 }
 
 TEST(Ellipse, PerimeterInt){
-    double abs = 0.0001;
+    double abs = 0.001;
     Shape* ellipse = new Ellipse(3, 4);
-    ASSERT_NEAR(21.1327, ellipse->perimeter(), abs);
+    ASSERT_NEAR(21.132, ellipse->perimeter(), abs);
 }
 
 TEST(Ellipse, PerimeterDouble){
-    double abs = 0.0001;
+    double abs = 0.001;
     Shape* ellipse = new Ellipse(3.7, 4.2);
-    ASSERT_NEAR(24.3893, ellipse->perimeter(), abs);
+    ASSERT_NEAR(24.389, ellipse->perimeter(), abs);
 }
 
 TEST(Ellipse, Type){
